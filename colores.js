c8ffe68bcefe4274967f268e9df78ee5fa73d898
abuelatata <artesaniaabuelatata@gmail.com>
2026-03-12 21:47:55 +0100

    Create colores.js

---
+++ colores.js
@@ -0,0 +1,13 @@
+const COLOR_MAP = {
+"00":"Blanco",
+"01":"Rosa",
+"04":"Celeste",
+"05":"Beige",
+"06":"Arena",
+"09":"Verde agua",
+"17":"Azul",
+"24":"Verde menta",
+"31":"Vino",
+"32":"Verde oliva",
+"34":"Verde fuerte"
+};
